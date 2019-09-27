1f5a7e326adc96579df081203314b895ecf1249e
Christian <chris@mejlak.com>
2019-09-27 18:00:36 +0200

    Setup CORS option

--- inc/function-redirect.php
+++ inc/function-redirect.php
@@ -5,3 +5,12 @@ if( !empty($frontend_url) && !is_admin() ) {
     wp_redirect($frontend_url);
     exit;
 }
+
+$allow_cors = (get_option('enable_cors') == 1 ? true : false);
+
+if( $allow_cors ) {
+    add_action( 'init', 'allow_origin' );
+        function allow_origin() {
+        header('Access-Control-Allow-Origin: '. $frontend_url);
+    }
+}
