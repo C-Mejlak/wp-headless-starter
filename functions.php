810a0c5ddb31cbe3beb9ec2c2ce2eddbf5792d52
Christian <chris@mejlak.com>
2019-09-28 11:30:59 +0200

    finish trigger netlify build

--- functions.php
+++ functions.php
@@ -4,3 +4,4 @@ add_theme_support( 'post-thumbnails' );
 
 require get_template_directory() . '/inc/function-admin.php';
 require get_template_directory() . '/inc/function-redirect.php';
+require get_template_directory() . '/inc/function-trigger-build.php';
--- inc/function-trigger-build.php
+++ inc/function-trigger-build.php
@@ -1,7 +1,11 @@
 <?php
 
-add_action( 'admin_enqueue_scripts', 'trigger_build_enqueue' );
-function trigger_build_enqueue() {
-    wp_enqueue_script( 'wp-trigger-netlify-build', plugin_dir_url( __FILE__ ) . '/js/headless-trigger-build.js', array(), '20190228', true );
-    wp_localize_script( 'wp-trigger-netlify-build', 'headlessTriggerBuildVars', get_option('trigger_url'));
+add_action( 'save_post', 'fireFunctionOnSave' );
+
+function fireFunctionOnSave($post_id) {
+    if (wp_is_post_revision($post_id) || wp_is_post_autosave($post_id)) {
+        return;
+    }
+    $trigger_url = get_option('trigger_url');
+    $response = Requests::post( $trigger_url );
 }
